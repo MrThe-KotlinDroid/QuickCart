7dca9ad35b7f8b07c9cec89f64a701797d263774
Abrar Hamim <abrarhamim2@gmail.com>
2025-04-25 20:31:27 +0600

    done

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,12 +1,12 @@
 import { inngest } from '@/config/inngest';
 import Product from '@/models/Product';
 import User from '@/models/User';
-import { auth } from '@clerk/nextjs/server';
+import { getAuth } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   try {
-    const { userId } = auth(request);
+    const { userId } = getAuth(request);
     const { address, items } = await request.json();
 
     if (!address || items.length === 0) {
